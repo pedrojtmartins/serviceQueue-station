45ab7b4414ff8e9e0bd30389685dc9f7921f4956
Pedro Martins <spjm.pt@hotmail.com>
2016-12-28 23:02:35 +0000

    Added images for layout

---
+++ QueueTerminal/Views/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using QueueTerminal.Controllers;
+using QueueTerminal.Models;
+using System.Windows;
+using System;
+
+namespace QueueTerminal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowController controller;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            controller = new MainWindowController(this);
+        }
+
+        public void NewListReceived(ServerUpdate update)
+        {
+        }
+
+        private async void Click_Next(object sender, RoutedEventArgs e)
+        {
+            await controller.RequestNextTicket();
+        }
+
+        public void UpdateCurrentTicket(string ticket)
+        {
+            currentTicket.Text = ticket;
+        }
+
+        public void DisplayNotInitialized()
+        {
+            
+        }
+    }
+}
